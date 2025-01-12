fa2996e503986319aa59c785760bc12790a084cb
Luis Angel Orduna Marquez <lorduna@d4.com.mx>
2025-01-12 02:58:28 -0600

    LAOM Se comodifican los estilos del sitio

--- src/app/layouts/full/sidebar/branding.component.ts
+++ src/app/layouts/full/sidebar/branding.component.ts
@@ -6,9 +6,10 @@ import { Component } from '@angular/core';
     <div class="branding">
       <a href="/">
         <img
-          src="./assets/images/logos/dark-logo.svg"
+          src="./assets/images/logos/LogoSuperior.png"
           class="align-middle m-2"
           alt="logo"
+          height="70px"
         />
       </a>
     </div>
